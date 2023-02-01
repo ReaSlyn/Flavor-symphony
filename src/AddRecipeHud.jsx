59b628542ce99a05e3deb22c1420c4ee4cb4cdaf
PokeStar04 <romain.monfret@hetic.net>
2023-02-01 10:18:14 +0100

    [ADD] Image in public folder & RecipeHud

--- src/AddRecipeHud.jsx
+++ src/AddRecipeHud.jsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 const AddRecipeHud = ({ delay, number }) => {
 	const [localDelay, setLocalDelay] = useState(delay);
+	console.log(number);
 
 	useEffect(() => {
 		const timer =
-			localDelay > 0 &&
-			setInterval(() => setLocalDelay((localDelay) => localDelay - 1), 10000);
+			localDelay > 0 && setInterval(() => setLocalDelay(localDelay - 1), 10000);
 		return () => clearInterval(timer);
 	}, [localDelay]);
 
 	return localDelay > 0 ? (
-		<></>
+		<div className="recipe__card-item" id="delayBar">
+			<img src={`recipes/Recipe${number}.png`} alt="recipe" />
+		</div>
 	) : (
 		<div className="recipe__card-item" id="delayBar">
 			<img src={`recipes/Recipe${number}.png`} alt="recipe" />
