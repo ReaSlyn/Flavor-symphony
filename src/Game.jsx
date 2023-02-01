690770c1dbf4f78e4edd2bc2cd29b9f893b83329
Thanh-Long Pham <thanh@theoffline.fr>
2023-02-01 11:10:21 +0100

    [RESOLVE] Bug where recipe wasn't generating

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,81 +1,70 @@
-import { useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import Experience from "./Experience.jsx";
-// import GetRecipe from './GetRecipe.jsx';
-import RecipeHud from "./RecipeHud.jsx";
-import GetRecipe from "./GetRecipev1.jsx";
-import Random from "./Helper/Random.jsx";
-
-// import AddRecipeHud from './AddRecipeHud.jsx';
-
-//counter
+import { useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience.jsx';
+import GetRecipe from './GetRecipe.jsx';
+import Random from './Helper/Random.jsx';
 
 export default function Game() {
-    const [isRunning, setIsRunning] = useState(false);
-    const [isPreviousSaveExisting, setPreviousSaveExisting] = useState(false);
+	const [isRunning, setIsRunning] = useState(false);
+	const [isPreviousSaveExisting, setPreviousSaveExisting] = useState(false);
 
-    const loadPreviousSave = () => {
-        /* @TODO load previous save */
-    };
+	const loadPreviousSave = () => {
+		/* @TODO load previous save */
+	};
 
-    //timer
-    const [counter, setCounter] = useState(2);
-    const [number, setNumber] = useState(Random(1, 8));
-    useEffect(() => {
-        if (counter > 0) {
-            const timer = setInterval(() => setCounter(counter - 1), 2000);
-            return () => clearInterval(timer);
-        }
-    }, [counter]);
-    if (counter === 0) {
-        const interval = setInterval(() => setNumber(Random(1, 8)), 8000);
-        setCounter(2);
-        return () => clearInterval(interval);
-    }
+	//timer
+	const [counter, setCounter] = useState(2);
+	const [number, setNumber] = useState(Random(1, 8));
+	useEffect(() => {
+		if (counter > 0) {
+			const timer = setInterval(
+				() => setCounter((counter) => counter - 1),
+				2000
+			);
+			return () => clearInterval(timer);
+		}
+	}, [counter]);
 
-    return (
-        <>
-            {!isRunning && (
-                <div className="menu">
-                    <img className="logo" src="./logo.png" alt="logo" />
-                    <div className="buttons">
-                        <div
-                            className="newgame"
-                            onClick={() => setIsRunning(true)}
-                        >
-                            <img
-                                src="./icon/new game.svg"
-                                alt="polygon start game"
-                            />
-                            <p>NEW GAME</p>
-                        </div>
-                        {isPreviousSaveExisting && (
-                            <div
-                                className="loadsave"
-                                onClick={() => loadPreviousSave()}
-                            >
-                                <p>LOAD SAVE</p>
-                            </div>
-                        )}
-                    </div>
-                </div>
-            )}
+	if (counter === 0) {
+		const interval = setInterval(() => setNumber(Random(1, 8)), 20000);
+		setCounter(2);
+		return () => clearInterval(interval);
+	}
 
-            {isRunning && (
-                <>
-                    <Canvas
-                        camera={{
-                            fov: 45,
-                            near: 0.1,
-                            position: [0, 24, 20],
-                        }}
-                    >
-                        <Experience />
-                    </Canvas>
-                    (counter === 0) ?
-                    <GetRecipe time={6} randomNumber={number} />:
-                </>
-            )}
-        </>
-    );
+	return (
+		<>
+			{!isRunning && (
+				<div className="menu">
+					<img className="logo" src="./logo.png" alt="logo" />
+					<div className="buttons">
+						<div className="newgame" onClick={() => setIsRunning(true)}>
+							<img src="./icon/new game.svg" alt="polygon start game" />
+							<p>NEW GAME</p>
+						</div>
+						{isPreviousSaveExisting && (
+							<div className="loadsave" onClick={() => loadPreviousSave()}>
+								<p>LOAD SAVE</p>
+							</div>
+						)}
+					</div>
+				</div>
+			)}
+
+			{isRunning && (
+				<>
+					<Canvas
+						camera={{
+							fov: 45,
+							near: 0.1,
+							position: [0, 24, 20],
+						}}
+					>
+						<Experience />
+					</Canvas>
+					(counter === 0) ?
+					<GetRecipe time={2} randomNumber={number} />:
+				</>
+			)}
+		</>
+	);
 }
--- src/GetRecipe.jsx
+++ src/GetRecipe.jsx
@@ -1,55 +1,46 @@
-import React, { useState, useEffect } from "react";
-import Random from "./Helper/Random";
-import RecipeHud from "./RecipeHud";
+import React, { useState, useEffect } from 'react';
+import Random from './Helper/Random';
+import RecipeHud from './RecipeHud';
 
 const GetRecipe = ({ time, randomNumber }) => {
-    const [recipe, setRecipe] = useState(null);
-    const [randomNumber, setRandomNumber] = useState(props.randomNumber);
+	const [recipe, setRecipe] = useState(null);
 
-    useEffect(() => {
-        const interval = setInterval(async () => {
-            try {
-                const res = await fetch(
-                    `http://localhost:5100/api/recipeFood/idRecipe=${randomNumber}`
-                );
-                const result = await res.json();
-                setRecipe(result);
-            } catch (error) {
-                console.error(error);
-            }
-        }, time * 1000);
-        return () => clearInterval(interval);
-    }, [randomNumber, time]);
+	useEffect(() => {
+		const interval = setInterval(async () => {
+			try {
+				const res = await fetch(
+					`http://localhost:5100/api/recipeFood/idRecipe=${randomNumber}`
+				);
+				const result = await res.json();
+				setRecipe(result);
+			} catch (error) {
+				console.error(error);
+			}
+		}, time * 1000);
+		return () => clearInterval(interval);
+	}, [randomNumber, time]);
 
-    return recipe ? (
-        <GetNewRecipe recipe={recipe} time={time} randomNumber={randomNumber} />
-    ) : null;
+	return recipe ? (
+		<GetNewRecipe recipe={recipe} time={time} randomNumber={randomNumber} />
+	) : null;
 };
 
 const GetNewRecipe = ({ recipe, time, randomNumber }) => {
-    const [counter, setCounter] = useState(time);
+	const [counter, setCounter] = useState(time);
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setCounter((prevCounter) => prevCounter - 1);
-        }, 10000);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCounter((prevCounter) => prevCounter - 1);
+		}, 10000);
 
-        return () => clearInterval(timer);
-    }, []);
+		return () => clearInterval(timer);
+	}, []);
 
-    return counter === 0 ? (
-        <RecipeHud
-            number={recipe.idRecipe}
-            time={props.time[0]}
-            randomNumber={randomNumber}
-        />
-    ) : (
-        <RecipeHud
-            number={recipe.idRecipe}
-            time={props.time[0]}
-            randomNumber={randomNumber}
-        />
-    );
+	return counter === 0 ? (
+		<RecipeHud time={time} randomNumber={randomNumber} />
+	) : (
+		<RecipeHud time={time} randomNumber={randomNumber} />
+	);
 };
 
 export default GetRecipe;
