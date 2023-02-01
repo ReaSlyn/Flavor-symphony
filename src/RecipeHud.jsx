a7da9de8825448f5f9c2479da485012c6ab5fcd8
PokeStar04 <romain.monfret@hetic.net>
2023-02-01 12:44:58 +0100

    [UPDATE]
    Change Image & Recipe generation 20s

--- src/RecipeHud.jsx
+++ src/RecipeHud.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
-import AddRecipeHud from './AddRecipeHud';
+import AddRecipeHud from "./AddRecipeHud";
 
 export default function RecipeHud({ time, randomNumber }) {
-	const [counter, setCounter] = useState(time);
+    const [counter, setCounter] = useState(time);
 
-	useEffect(() => {
-		if (counter > 0) {
-			const timer = setInterval(
-				() => setCounter((counter) => counter - 1),
-				5000
-			);
-			return () => clearInterval(timer);
-		}
-	}, [counter]);
+    useEffect(() => {
+        if (counter > 0) {
+            const timer = setInterval(
+                () => setCounter((counter) => counter - 1),
+                5000
+            );
+            return () => clearInterval(timer);
+        }
+    }, [counter]);
 
-	return counter === 0 ? (
-		<div className="recipe_container">
-			<div className="recipe__flex">
-				<AddRecipeHud number={randomNumber} delay={2} />
-			</div>
-		</div>
-	) : (
-		<div className="recipe_container">
-			<div className="recipe__flex">
-				<AddRecipeHud number={randomNumber} delay={2} />
-			</div>
-		</div>
-	);
+    return counter === 0 ? (
+        <div className="recipe_container">
+            <div className="recipe__flex">
+                <AddRecipeHud number={randomNumber} delay={2} />
+            </div>
+        </div>
+    ) : (
+        <div className="recipe_container">
+            <div className="recipe__flex">
+                <AddRecipeHud number={randomNumber} delay={2} />
+            </div>
+        </div>
+    );
 }
